11baee2abf051b730447ac5bf73a47e3a9dfe076
sidweb00 <66740260+sidweb00@users.noreply.github.com>
2022-01-26 22:13:20 +0530

    comments

--- asteriod.py
+++ asteriod.py
@@ -1,9 +1,9 @@
-# implementation of Spaceship - program template for RiceRocks
+
 import simplegui
 import math
 import random
 
-# globals for user interface
+
 WIDTH = 800
 HEIGHT = 600
 score = 0
@@ -40,12 +40,11 @@ class ImageInfo:
 
     
     
-# debris images - debris1_brown.png, debris2_brown.png, debris3_brown.png, debris4_brown.png
-#                 debris1_blue.png, debris2_blue.png, debris3_blue.png, debris4_blue.png, debris_blend.png
+
 debris_info = ImageInfo([320, 240], [640, 480])
 debris_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris2_blue.png")
 
-# nebula images - nebula_brown.png, nebula_blue.png
+
 nebula_info = ImageInfo([400, 300], [800, 600])
 nebula_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.f2014.png")
 
@@ -57,29 +56,27 @@ splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/cod
 ship_info = ImageInfo([45, 45], [90, 90], 35)
 ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
 
-# missile image - shot1.png, shot2.png, shot3.png
+# missile image
 missile_info = ImageInfo([5,5], [10, 10], 3, 50)
 missile_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/shot2.png")
 
-# asteroid images - asteroid_blue.png, asteroid_brown.png, asteroid_blend.png
+# asteroid images 
 asteroid_info = ImageInfo([45, 45], [90, 90], 40)
 asteroid_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blue.png")
 
-# animated explosion - explosion_orange.png, explosion_blue.png, explosion_blue2.png, explosion_alpha.png
+# animated explosion 
 explosion_info = ImageInfo([64, 64], [128, 128], 17, 24, True)
 explosion_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/explosion_alpha.png")
 
-# sound assets purchased from sounddogs.com, please do not redistribute
-# .ogg versions of sounds are also available, just replace .mp3 by .ogg
+
+
 soundtrack = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/soundtrack.mp3")
 missile_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3")
 missile_sound.set_volume(.5)
 ship_thrust_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3")
 explosion_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/explosion.mp3")
 
-# alternative upbeat soundtrack by composer and former IIPP student Emiel Stopler
-# please do not redistribute without permission from Emiel at http://www.filmcomposer.nl
-#soundtrack = simplegui.load_sound("https://storage.googleapis.com/codeskulptor-assets/ricerocks_theme.mp3")
+
 
 
 # helper functions to handle transformations
